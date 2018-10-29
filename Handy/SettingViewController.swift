f2475aa6e86491a90c4d000dcb0357dea2343bc8
hongjian lin <hongjianlin@hongjians-MacBook-Pro.local>
2018-10-29 14:28:44 -0400

    setup tap navigation for the app

---
+++ Handy/SettingViewController.swift
@@ -0,0 +1,32 @@
+
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+//        #if DEVELOPMENT
+//        self.testLabel.text = "This is DEVELOPMENT Build"
+//        self.testLabel.textColor = UIColor.blue
+//        #elseif DISTRIBUTION
+//        self.testLabel.text = "This is DISTRIBUTION Build"
+//        self.testLabel.textColor = UIColor.magenta
+//        #else
+//        self.testLabel.text = "This is OTHER Build"
+//        self.testLabel.textColor = UIColor.red
+//        #endif
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
